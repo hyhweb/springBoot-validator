d95b75f1ccc0e60483f7dc4f0b29d5641a3a40a7
hyhweb <461342854@qq.com>
2020-06-09 17:55:34 +0800

    code

--- src/main/java/com/springboot/validator/controller/OrderController.java
+++ src/main/java/com/springboot/validator/controller/OrderController.java
@@ -3,18 +3,22 @@ package com.springboot.validator.controller;
 import com.alibaba.fastjson.JSON;
 import com.springboot.validator.bean.BeanValidator;
 import com.springboot.validator.entity.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
   @GetMapping("/save")
   public String save(@RequestBody String json) {
     Order order = JSON.parseObject(json, Order.class);
     BeanValidator.validate(order);
+    log.info(order.toString());
     return order.toString();
   }
 
@@ -22,6 +26,7 @@ public class OrderController {
   public String add(@RequestBody String json) {
     Order order = JSON.parseObject(json, Order.class);
     BeanValidator.validate(order, Order.Create.class);
+    log.info(JSON.toJSONString(order));
     return order.toString();
   }
 
@@ -29,6 +34,7 @@ public class OrderController {
   public String update(@RequestBody String json) {
     Order order = JSON.parseObject(json, Order.class);
     BeanValidator.validate(order, Order.Update.class);
+    log.info("{}", order);
     return order.toString();
   }
 }
